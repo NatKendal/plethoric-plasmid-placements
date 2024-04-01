f5b1ae50b41dbfd829fa4ccea46fd3decc350192
NatKendal <nmkendal@uwaterloo.ca>
2024-04-01 09:46:15 -0400

    colour function to gaussian

--- plplpl/colour_delay_functions/naive.py
+++ plplpl/colour_delay_functions/naive.py
@@ -2,18 +2,16 @@
 
 import math
 
-# input: minimum, maximum of range we care about, scaling of exponential distribution
-# output: dictionary of exponential distribution values for all time steps in the range 
-
-# IMPORTANT: this implementation gives a probability of 0 at the minimum -- may need to adjust this
-
-def calcValues(scale=1/3, min=3, max=18):
+# input: minimum, maximum of range we care about, 
+# average and standard deviation of normal distribution 
+# output: dictionary of normal distribution values for all time steps in the range 
+def calcValues(avg=20, std=3, min=11, max=29):
 
     vals = dict()
 
     for i in range(min, max):
 
-        vals[i] = 1 - math.exp(-1 * scale * (i-min))
+        vals[i] = 0.5 * (1 + math.erf( (i - avg) / (std * math.sqrt(2)) ))
 
     # force maximum value to be 1
     vals[max] = 1
@@ -21,7 +19,7 @@ def calcValues(scale=1/3, min=3, max=18):
     return vals
 
 # input: time step of cell we are looking at and ancestor, dictionary of values in range
-# output: probability the cell we are looking at is yellow, given that ancestor has gene
+# output: probability the cell we are looking at is active, given that ancestor has gene
 def probColour(tCell, tAncestor, dictValues):
 
     return ( dictValues.get(tCell-tAncestor, 0) )
