090d8b301ee913410eb38bd08447111b48ee79b3
NatKendal <nmkendal@uwaterloo.ca>
2024-04-29 14:32:09 -0400

    basic contact weight - untested

---
+++ plplpl/conjugation_functions/contact_weights.py
@@ -0,0 +1,20 @@
+# determining the quality of contact between cells
+
+# input: list of all cells, dictionary of all neighbours, later will include other info
+# output: dictionary mapping cell pairs to the weight of the edge from cell1 to cell2 (cell1,cell2)
+def getContactWeights(all_cells, dictNeighbours):
+
+    contact_weights = dict()
+
+    # iterate through each cell
+    for cell1 in all_cells:
+        # and check the weight for all of its neighbors
+        for cell2 in dictNeighbours[cell1]:
+            contact_weights[(cell1,cell2)] = calcWeight(cell1,cell2)
+
+    return contact_weights
+
+# input: two cells, later will include other info
+# output: weight of edge from first cell to second cell
+def calcWeight(cell1,cell2):
+    return 1/1000
